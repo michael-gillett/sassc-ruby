9ef48616056a806a28a046df67b0c310a67cbb12
Baptiste Benet <baptiste.benet@liveramp.com>
2017-01-24 16:14:53 -0800

    Changing sass

--- app/assets/javascripts/components/goLinksCreateForm.jsx
+++ app/assets/javascripts/components/goLinksCreateForm.jsx
@@ -9,44 +9,45 @@ var GoLinksCreateForm = React.createClass ({
   render () {
     return (
       <div className="container">
-      <div className="row">
-        <UiHeader
-          textTitle="Go Links"
-          helpText="Create a Go/ link through this page"
-        />
-        <UiInput
-          inputValue={this.props.goLinks.alias}
-          label={"Alias"}
-          placeholder={""}
-          valid={!this.props.goLinks.alias}
-          handleChange={ (e) => { this.props.goLinksActions.setAlias(e.target.value); } }
-          labelShift={false}
-        />
-        <UiInput
-          inputValue={this.props.goLinks.url}
-          label={"URL"}
-          placeholder={""}
-          valid={!this.props.goLinks.url || ValidURL.isUri(this.props.goLinks.url)}
-          handleChange={ (e) => { this.props.goLinksActions.setUrl(e.target.value); } }
-          labelShift={false}
-        />
-        <UiInput
-          inputValue={this.props.goLinks.description}
-          label={"Description"}
-          placeholder={""}
-          valid={!this.props.goLinks.description}
-          handleChange={ (e) => { this.props.goLinksActions.setDescription(e.target.value); } }
-          labelShift={false}
-        />
-      </div>
-      <div className="row">
-        <button onClick={() => this.props.goLinksActions.redirect("/")} className="button">
-          Back
-        </button>
-        <button onClick={() => {}} className="button">
-          Save
-        </button>
-      </div>
+        <div className="row">
+          <UiHeader
+            textTitle="Go Links"
+            helpText="Create a Go/ link through this page"
+          />
+          <UiInput
+            inputValue={this.props.goLinks.newGoLinkData.alias}
+            label={"Alias"}
+            placeholder={""}
+            valid={this.props.goLinks.newGoLinkData.alias}
+            handleChange={ (e) => { this.props.goLinksActions.setAlias(e.target.value); } }
+            labelShift={false}
+          />
+          <UiInput
+            inputValue={this.props.goLinks.newGoLinkData.url}
+            label={"URL"}
+            placeholder={""}
+            valid={!this.props.goLinks.newGoLinkData.url || ValidURL.isWebUri(this.props.goLinks.newGoLinkData.url)}
+            handleChange={ (e) => { this.props.goLinksActions.setUrl(e.target.value); } }
+            labelShift={false}
+          />
+          <UiInput
+            id="description"
+            inputValue={this.props.goLinks.newGoLinkData.description}
+            label={"Description"}
+            placeholder={""}
+            valid={true}
+            handleChange={ (e) => { this.props.goLinksActions.setDescription(e.target.value); } }
+            labelShift={false}
+          />
+        </div>
+        <div className="row">
+          <button onClick={() => this.props.goLinksActions.redirect("/")} className="button">
+            Back
+          </button>
+          <button onClick={() => {}} className="button">
+            Save
+          </button>
+        </div>
       </div>
     );
   }
