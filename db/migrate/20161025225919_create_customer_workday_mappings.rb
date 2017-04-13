739bae243c49dfd0be6c6b680cadbc701fe4f7c2
Shrif Nada <shrif.nada@liveramp.com>
2017-04-12 18:35:31 -0700

    plox fix build

---
+++ db/migrate/20161025225919_create_customer_workday_mappings.rb
@@ -0,0 +1,11 @@
+class CreateCustomerWorkdayMappings < ActiveRecord::Migration
+  def change
+  #   create_table :customer_workday_mappings do |t|
+  #     t.integer :liveramp_customer_id, null: false
+  #     t.string :workday_id, null: false
+
+  #     t.timestamps null: false
+  #   end
+  #   add_index :customer_workday_mappings, :liveramp_customer_id, unique: true
+  end
+end
