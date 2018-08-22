db7ad58e2596646114e50e610d402d0c633bb9d9
Lane Erickson <lerickson@liveramp.com>
2018-08-22 04:32:13 -0700

    gsub _ with - before save

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,9 @@
 class Link < ActiveRecord::Base
   validates :url, url: true
+  before_save :change_alias_underscores_to_dashes
+
+  private
+  def change_alias_underscores_to_dashes
+    self.alias.gsub!("_", "-")
+  end
 end
